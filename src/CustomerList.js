8715771b08f09accb2b0fce58b61bbd6321b5aa8
PutkisDude <putkis@gmail.com>
2021-11-02 09:56:53 +0200

    idk

--- src/CustomerList.js
+++ src/CustomerList.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import {AgGridReact} from 'ag-grid-react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham-dark.css';
 
 function CustomerList() {
 
@@ -23,12 +23,13 @@ function CustomerList() {
         resizable:true, 
         sortable:true, 
         filter:true, 
-        lockPosition: true
+        lockPosition: true,
+        floatingFilter: true
     }
     
     const columns = [
-        {headerName: 'Person information', children: [
-            {headerName: 'First name',lockPosition: true, field: 'firstname', width:150},
+        {headerName: 'Person information', floatingFilter:true, children: [
+            {headerName: 'First name',lockPosition: true, floatingFilter:true,field: 'firstname', width:150},
             {headerName: 'Last name', field: 'lastname', width: 150},
         ]},
         {headerName: 'Contact', children: [
@@ -37,13 +38,13 @@ function CustomerList() {
         ]
         },
         {headerName: 'Address', children: [
-            {field: 'city', width:120, columnGroupShow: 'close'},
-            {headerName:'Street Address', field: 'streetaddress', columnGroupShow: 'open'},
-            {field: 'postcode', columnGroupShow: 'open'}] }
+            {field: 'city', width:120,  columnGroupShow: 'close'},
+            {headerName:'Street Address', filter:false,field: 'streetaddress', columnGroupShow: 'open'},
+            {field: 'postcode', filter:false,columnGroupShow: 'open'}] }
     ]
 
     return(
-            <div className="ag-theme-alpine-dark fullheight">
+            <div className="ag-theme-balham-dark fullheight">
                 <AgGridReact
                 rowData={customers}
                 columnDefs={columns}
